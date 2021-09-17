7b172fbb5bc3691e912afaff28dd5ccb21dca0fc
Alexmazhara <58631876+Alexmazhara@users.noreply.github.com>
2021-09-17 09:06:27 +0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 64521;
+	printf("a= %d\n",a );
+	unsigned b = 14141;
+	printf("b= %d\n",b );
+	long c = 1564651;
+	printf("c= %d\n",c );
+	unsigned long d = 45884;
+	printf("d= %d\n",d );
+	float e = 4565610;
+	printf("e= %d\n",e );
+	double f = 121654652;
+	printf("f= %d\n", f);
+	long double g = 45546564215;
+	printf("g= %d\n", g);
+
+	/*void;
+	printf("%d\n", );*/
+
+	char i = 126;
+	printf("i= %d\n",i );
+	unsigned char j = 254;
+	printf("j= %d\n",j );
+	short k = 32765;
+	printf("k= %d\n",k );
+	unsigned short l = 65534;
+	printf("l= %d\n",l );
+
+	// printf("%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d", a, b, c, d, i, f, g, i, j, k, l);
+	return 0;
+
+}
